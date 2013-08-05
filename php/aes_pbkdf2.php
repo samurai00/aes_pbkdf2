3cf0ead72b56f7dec0b39dc357aa80d0cf679c10
afon <wengxufeng@gmail.com>
2013-08-05 17:08:55 +0800

    Add/Strip PKCS7 padding for php

--- php/aes_pbkdf2.php
+++ php/aes_pbkdf2.php
@@ -18,6 +18,11 @@ function rand_str($length) {
 }
 
 function aes_encrypt($data, $password) {
+    // Add PKCS7 padding.
+    $block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    if (($pad = $block - (strlen($data) % $block)) < $block) {
+      $data .= str_repeat(chr($pad), $pad);
+    }
     $salt = rand_str(SALT_LEN);
     $encrypted = mcrypt_encrypt(
         MCRYPT_RIJNDAEL_128,
@@ -32,11 +37,18 @@ function aes_encrypt($data, $password) {
 function aes_decrypt($data, $password) {
     $salt = substr($data, 0, SALT_LEN);
     $data = substr($data, SALT_LEN);
-    return mcrypt_decrypt(
+    $decrypted = mcrypt_decrypt(
         MCRYPT_RIJNDAEL_128,
         hash_pbkdf2(KET_ALGO, $password, $salt, ITERATIONS, KEY_LENGTH, true),
         base64_decode($data),
         MCRYPT_MODE_CBC,
         "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
     );
+    // Strip padding out.
+    $block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    $pad = ord(substr($decrypted, -1));
+    if ($pad && $pad < $block && preg_match('/'.chr($pad).'{'.$pad.'}$/', $decrypted)) {
+        return substr($decrypted, 0, strlen($decrypted) - $pad);
+    }
+    return $decrypted;
 }
