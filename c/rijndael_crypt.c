2aad8860aeb08c4b4822e35d8a2bd51facd154f0
afon <wengxufeng@gmail.com>
2014-06-03 18:08:33 +0800

    fix PKCS7 padding in C

--- c/rijndael_crypt.c
+++ c/rijndael_crypt.c
@@ -23,12 +23,10 @@ void rijndael256Encrypt(const char *plaindata, unsigned char password[KEYLENGTH(
     unsigned char new_plaindata[data_len];
     memcpy(new_plaindata, plaindata, data_len);
     unsigned int pad = 16 - (data_len%16);
-    if (pad < 16) {
-        int y = 0;
-        for (; y < pad; new_plaindata[data_len + y++] = (char)pad);
-        new_plaindata[data_len + y + 1] = 0;
-        data_len = data_len + y;
-    }
+    int y = 0;
+    for (; y < pad; new_plaindata[data_len + y++] = (char)pad);
+    new_plaindata[data_len + y + 1] = 0;
+    data_len = data_len + y;
 
     nrounds = rijndaelSetupEncrypt(rk, key, KEYBITS);
     unsigned char iv[16];
@@ -78,12 +76,10 @@ void rijndael256Decrypt(const char *cipherdata, unsigned char password[KEYLENGTH
     // Strip PKCS7 padding.
     unsigned int padding = 1;
     unsigned int pad = tmp_plaindata[data_len - 1];
-    if (pad < 16) {
-        for (int i = 0; i < pad; i++) {
-            if ((unsigned int)tmp_plaindata[data_len - i - 1] != pad) {
-                padding = 0;
-                break;
-            }
+    for (int i = 0; i < pad; i++) {
+        if ((unsigned int)tmp_plaindata[data_len - i - 1] != pad) {
+            padding = 0;
+            break;
         }
     }
     if (padding == 1) {
